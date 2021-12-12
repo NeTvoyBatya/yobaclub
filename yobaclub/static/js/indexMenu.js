e4f87fdb88a45824c6b40909b8eed1dee6546838
Nikita <nikita.medvedew1@gmail.com>
2021-12-12 18:16:46 +0500

    Bibabubip

--- yobaclub/static/js/indexMenu.js
+++ yobaclub/static/js/indexMenu.js
@@ -1,4 +1,6 @@
 var closeMainMenu = document.getElementById("CloaseMainMenu")
+var openMainMenu = document.getElementById("openMainMenu")
+var CloseButton = document.getElementById("CloseButton")
 
 
 function LinkFunction(element){
@@ -7,5 +9,18 @@ function LinkFunction(element){
     let openLink = document.getElementById("OpenLink"+number)
     link.classList.toggle("on")
     closeMainMenu.classList.toggle("on")
+    openMainMenu.classList.toggle("on")
     openLink.classList.toggle("on")
+    CloseButton.classList.toggle("on")
+}
+
+function closeButton(){
+    for (element of document.getElementsByClassName('openLink')) {
+        if (element.className.endsWith(' on')){
+            element.classList.toggle('on')
+        }
+    }
+    closeMainMenu.classList.toggle("on")
+    openMainMenu.classList.toggle("on")
+    CloseButton.classList.toggle("on")
 }
