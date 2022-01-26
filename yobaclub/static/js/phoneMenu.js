e22bdf4b22c84c13cb0f75a28182f027f0472bf7
Nikita <nikita.medvedew1@gmail.com>
2022-01-26 22:35:06 +0500

    phone adaptation

---
+++ yobaclub/static/js/phoneMenu.js
@@ -0,0 +1,10 @@
+var PhoneMenuId = document.getElementById('PhoneMenuId');
+var PhoneNavId = document.getElementById('phone__navId')
+var BGBlur = document.getElementById('mainContainer')
+var Body = document.getElementById('body')
+function PhoneMenu (){
+    PhoneMenuId.classList.toggle("on")
+    PhoneNavId.classList.toggle("on")
+    BGBlur.classList.toggle("blur")
+    Body.classList.toggle("nonScroll")
+}
