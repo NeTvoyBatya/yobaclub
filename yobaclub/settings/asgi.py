63c7d34f04caa1c0490dac764c6e3c8350953d63
Alexey <more.fune@mail.com>
2022-02-13 17:29:19 +0500

    heroku deployment 7

--- yobaclub/settings/asgi.py
+++ yobaclub/settings/asgi.py
@@ -3,10 +3,11 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
 from channels.auth import AuthMiddlewareStack
 from yobaclub.websockets import websockets
-
+from django import setup
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yobaclub.settings')
 
+setup()
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
